be6b6917991f94ef2aaeecd7a09b63f73a105208
arlan <arlannomad@gmail.com>
2024-02-10 09:44:21 +0600

    Refactored ChatGptRequest.java

--- src/main/java/kz/almaty/telegrambotspringboot/dto/ChatGptRequest.java
+++ src/main/java/kz/almaty/telegrambotspringboot/dto/ChatGptRequest.java
@@ -1,6 +1,9 @@
 package kz.almaty.telegrambotspringboot.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +12,10 @@ import java.util.List;
 public class ChatGptRequest {
     private String model;
     private List<Message> messages;
+
+    private double temperature = 0.9;
+    @JsonProperty(value = "max_tokens")
+    private int maxTokens = 256;
 
     public ChatGptRequest(String model, String prompt) {
         this.model = model;
